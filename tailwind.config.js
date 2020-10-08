0b75d34f8c7d95e584377ec444e6e05deeb450ba
Andreas Keller <andreas.keller@gmx.ch>
2020-10-08 20:39:45 +0200

    Hopefully fixes purgeing issues

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,32 @@ const rem = (px) => `${round(px / 16)}rem`;
 const em = (px, base) => `${round(px / base)}em`;
 
 module.exports = {
-  purge: ["./src/**/*.{js,mdx}", "./next.config.js"],
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: {
+    content: ["./src/**/*.{js,mdx}", "./next.config.js"],
+    options: {
+      extractors: [
+        {
+          extensions: ["mdx"],
+          extractor: (content) => {
+            content = mdx.sync(content);
+
+            // Capture as liberally as possible, including things like `h-(screen-1.5)`
+            const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+            // Capture classes within other delimiters like .block(class="w-1/2") in Pug
+            const innerMatches =
+              content.match(/[^<>"'`\s.(){}[\]#=%]*[^<>"'`\s.(){}[\]#=%:]/g) || [];
+
+            return broadMatches.concat(innerMatches);
+          },
+        },
+      ],
+    },
+  },
   theme: {
     extend: {
       spacing: {
